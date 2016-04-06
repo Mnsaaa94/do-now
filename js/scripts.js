6d8ad7c15a4cc48fea17f67465bde457f6494b04
Mnsaaa94 <7mnsaaa94@gmail.com>
2016-04-06 09:32:04 -0400

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(".animate-on-mouseenter").on("mouseenter", function() {
 	$(this).toggleClass("animated bounce");
-});
+});.on("mouseleave")
+$(".animate-on-mouseenter-2").on("mouseenter", function() {
+	$(this).toggleClass("animated flipInX");
+});.on("mouseleave")
+$(".animate-on-mouseenter-3").on("mouseenter", function() {
+	$(this).toggleClass("animated rubberBand");
+});.on("mouseleave")
